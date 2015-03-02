15921bf2c38095f90bb3acea55569ad2af508b52
EC2 Default User <ec2-user@ip-172-31-2-39.eu-central-1.compute.internal>
2015-03-02 17:03:46 +0000

    iphone communication update for AWS server

--- FoodPlazaPage/bin/emailManagement.js
+++ FoodPlazaPage/bin/emailManagement.js
@@ -31,7 +31,7 @@ var sendConfirmationMail = function(user, err){
     to:  user.email,
     subject: 'Food Plaza Confirmation',
     text: '',
-    html: '<div style="width:500px;height:200px;background-color:rgba(0, 0, 0, 0.8);color:white;border-radius:20px;font-size:20px;text-align:center;padding:80px;">Hello ' + user.firstname + '</br>Please register your Email by clicking the link beneath!</br></br><a href="http://localhost:3000/verify/' + user.userid + '/' + user.token + '">Confirm your Food Plaza Account</a></div>'
+    html: '<div style="width:500px;height:200px;background-color:rgba(0, 0, 0, 0.8);color:white;border-radius:20px;font-size:20px;text-align:center;padding:80px;">Hello ' + user.firstname + '</br>Please register your Email by clicking the link beneath!</br></br><a href="http://ec2-54-93-47-16.eu-central-1.compute.amazonaws.com/verify/' + user.userid + '/' + user.token + '">Confirm your Food Plaza Account</a></div>'
     };
     
     transporter.sendMail(mailOptions, function(error, info){
